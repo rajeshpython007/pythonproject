7c64d9d502d2752c69f50174fdbb7bf61469cd55
raja <rajeshh.nicmar@gmail.com>
2022-02-19 16:16:34 +0530

    my project

--- pandas.py
+++ pandas.py
@@ -12,3 +12,21 @@ print(a)
 
 a=6;b=7;v=7;
 print(a+b)
+
+a=5;c=6;d="mujob";
+print(d)
+print('the value is',d)
+
+numbers=[10,20,30,40,50,60,60,70,80,90,100]
+print(numbers[3:5])
+print (numbers[2::2])
+
+lst=[1,20,5,6,4,2]
+lst.sort()
+print(lst)
+slist=sorted(lst)
+print(slist)
+
+s='mine groung'
+slist=s.split()
+print(slist)
