6249cb001a928761e1603326ae855d1cf88917ba
raja <rajeshh.nicmar@gmail.com>
2022-02-19 16:04:31 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pythonproject
+# pythonproject 1
---
+++ pandas.py
@@ -0,0 +1,14 @@
+mystring="mypreparation"
+print(mystring)
+
+mystring1="mypreparatinn"
+print(mystring1)
+
+
+a=4
+b=8
+c=7
+print(a)
+
+a=6;b=7;v=7;
+print(a+b)
